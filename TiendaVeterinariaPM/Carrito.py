e2b3e7a7cc7a4c5b409d183408725f4c797f10fc
Angelo Muñoz <angelopatricio.munoz@alumnos.ulagos.cl>
2023-04-04 19:21:32 -0400

    Subiendo Carrito y Usuario

---
+++ TiendaVeterinariaPM/Carrito.py
@@ -0,0 +1,20 @@
+class Carrito():
+    
+    def __init__(self, articulos, total_precio, cant_productos):
+        self._articulos = articulos
+        self._total_precio = total_precio
+        self._cant_productos = cant_productos
+        
+    articulos = []
+    
+    def AgregarArticulo():
+        
+    def EliminarArticulo():
+        
+    def VerificarStockArticulos():
+        
+    def CalcularTotal():
+        
+    def ObtenerCantidadArticulos():
+        
+    def VerificarClienteFrecuente():
