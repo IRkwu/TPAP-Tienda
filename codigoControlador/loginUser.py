30e0d81fd69a007d8bbb1c5b09aff578987127b5
VictorCalbucura <111377379+VictorCalbucura@users.noreply.github.com>
2023-05-13 15:03:24 -0400

    creando modelo vista controlador ui y codigos

---
+++ codigoControlador/loginUser.py
@@ -0,0 +1,22 @@
+import typing
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
+import sys
+from codigoVista.uiLoginUser import uiLogin
+
+class login(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ventana = uiLogin()
+        self.ventana.setupUi(self)
+        
+        self.ventana.btnIngresar.clicked.connect(self.metodoEjemplo)
+        
+    def metodoEjemplo(self):
+        print("ola")
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    ventanaMain = login()
+    ventanaMain.show()
+    app.exec_()
