e40e64a47c5415b042d209197abcd689fb784b5c
VictorCalbucura <111377379+VictorCalbucura@users.noreply.github.com>
2023-05-29 00:27:28 -0400

    Ultimos arreglos verificar Stock
    Se arreglan los detalles del codigo, entre ellos, verificar Stock, lista productos, entre otros

--- VerificarStock.py
+++ VerificarStock.py
@@ -1,24 +1,37 @@
 import csv
-from PyQt5.QtWidgets import QMainWindow, QApplication
+import typing
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
 import PyQt5.QtWidgets as qtw
 import sys
 from UiVerificar import Ui_VerificarProd
 import listaProd
 from csvArchivo import csvArch
-class Verificar(QMainWindow):
-        def __init__(self, cont):
+import uiVentEmer
+
+class Emergente(QMainWindow):
+    def __init__(self):
            super().__init__()
+           self.vent = uiVentEmer.uiVentEm()
+           self.vent.setupUi(self)
+           
+    def closeEvent(self, event):
+        event.ignore()
+
+class Verificar(QMainWindow):
+        def __init__(self, cont, usuario):
+           super().__init__()
+           p = True
            self.cont = cont
-           contVerificar = 0 #Numero del producto
-           self.contVerificar = contVerificar
+           self.usuario = usuario
            self.ventana = Ui_VerificarProd()
            self.ventana.setupUi(self)
-           self.ventana.btnAtras.clicked.connect(self.Volver)
-           self.ventana.botonAgregar.clicked.connect(self.agregarStock)
-           self.ventana.botonGuardar.clicked.connect(self.guardarStock)
+           self.vent = listaProd.listaProd(self.usuario)
+           self.ventana.btnAtras.clicked.connect(lambda: self.Volver(self.vent))
+           self.ventanaEm = Emergente()
+           self.ventana.botonAgregar.clicked.connect(lambda: self.agregar())
+           self.ventanaEm.vent.btnGuardar.clicked.connect(lambda: self.guardarStock(self.ventanaEm))
            
-           
-           print(self.contVerificar)
 
         def actualizar(self):
                    with open('Archivos de Datos/ListaArticulos.csv', "r", encoding="ISO 8859-1") as r:
@@ -27,45 +40,39 @@ class Verificar(QMainWindow):
                     i = 0
                     for l in lector:
                         i = (i+1)
-                        if i == (self.contVerificar+1):
-                            Producto = l
+                        if i == (self.cont+1):
+                            self.Producto = l
 
                     
-                    self.ventana.labelid.setText(str(Producto[2]))
-                    self.ventana.labelNombre.setText(str(Producto[0]))
-                    self.ventana.labelMascota.setText(str(Producto[1]))
-                    self.ventana.labelMarca.setText(str(Producto[3]))
-                    self.ventana.labelCategoria.setText(str(Producto[7]))
-                    self.ventana.labelPrecio.setText(str(Producto[4]))
-                    self.ventana.labelPrecioLote.setText(str(Producto[8]))
-                    self.ventana.inputStock.setText(str(Producto[5]))
+                    self.ventana.labelid.setText(str(self.Producto[2]))
+                    self.ventana.labelNombre.setText(str(self.Producto[0]))
+                    self.ventana.labelMascota.setText(str(self.Producto[1]))
+                    self.ventana.labelMarca.setText(str(self.Producto[3]))
+                    self.ventana.labelCategoria.setText(str(self.Producto[7]))
+                    self.ventana.labelPrecio.setText(str(self.Producto[4]))
+                    self.ventana.labelPrecioLote.setText(str(self.Producto[8]))
                     self.ventana.labelStockEditable.setVisible(True)
-                    self.ventana.inputStock.setVisible(False)
-                    self.ventana.labelStockEditable.setText(str(Producto[5]))
-                    
-
-        def agregarStock(self):
-            self.ventana.labelStockEditable.setVisible(False)
-            self.ventana.inputStock.setVisible(True)
+                    self.ventana.labelStockEditable.setText(str(self.Producto[5]))
         
+        def agregar(self):
+            self.setEnabled(False)
+            self.ventanaEm.vent.spinBox.setValue(1)
+            self.ventanaEm.vent.spinBox.setMinimum(1)
+            self.ventanaEm.vent.spinBox.setMaximum(99)
+            self.ventanaEm.show()
         
-        def guardarStock(self):
-            nuevo_stock = self.ventana.inputStock.text()
-            csvArch.modificar("Archivos de Datos/ListaArticulos.csv")
+        def guardarStock(self, vent):
+            stock = self.ventanaEm.vent.spinBox.value()
+            stockFinal = int(self.Producto[5]) + stock
+            csvArch.modificar("Archivos de Datos/ListaArticulos.csv", self.cont, 5, stockFinal)
+            vent.hide()
             self.actualizar()
+            self.setEnabled(True)
    
-        
            
        
-        def Volver(self):
-            self.listaProd = listaProd.listaProd(self.cont)
-            self.listaProd.show()
-            self.hide() 
-            
-                 
-                
-if __name__=="__main__":
-    app = QApplication(sys.argv)
-    ventanaMain = Verificar()
-    ventanaMain.show()
-    app.exec_()                
+        def Volver(self, ventana):
+            ventana.actualizarNombre()
+            ventana.actualizarLista()
+            ventana.show()
+            self.hide()
--- listaProd.py
+++ listaProd.py
@@ -1,9 +1,11 @@
 import csv
 from PyQt5.QtWidgets import QMainWindow, QApplication
 import PyQt5.QtWidgets as qtw
-import sys
+import PyQt5.QtGui as qtg
+import PyQt5.QtCore as qc
 from uiListaProd import uiListaP
 import VerificarStock
+import loginUser
 
 
 class listaProd(QMainWindow):
@@ -14,7 +16,11 @@ class listaProd(QMainWindow):
         self.ventana.setupUi(self)
         self.actualizarLista()
         self.lista = self.ventana.listaProductos
+        self.ventana.btnRevisarProd.setEnabled(False)
         self.ventana.btnRevisarProd.clicked.connect(self.Ingresar)
+        self.lista.itemSelectionChanged.connect(self.filaSel)
+        self.ventana.btnCerrar.clicked.connect(lambda: self.cerrar())
+        self.ventana1 = loginUser.login()
     
     def actualizarNombre(self):
         self.ventana.labelUsuarioEnLogin.setText(self.usuario[1])
@@ -43,6 +49,17 @@ class listaProd(QMainWindow):
             
             precio = qtw.QTableWidgetItem("$" + prod[4])
             self.ventana.listaProductos.setItem(i, 4, precio)
+        
+            # Crear etiqueta cuadrada verde
+            etiqueta = qtw.QTableWidgetItem()
+            if int(prod[9]) >= int(prod[5]):
+                etiqueta.setBackground(qtg.QColor('red'))
+            else:
+                etiqueta.setBackground(qtg.QColor('green'))
+            etiqueta.setTextAlignment(qc.Qt.AlignCenter)
+            etiqueta.setFlags(qc.Qt.ItemIsEnabled)
+            self.ventana.listaProductos.setItem(i, 5, etiqueta)
+            
 
         with open('Archivos de Datos/ListaUsuarios.csv', 'r', encoding="utf-8") as r:
             l = csv.reader(r, delimiter=",")
@@ -56,12 +73,23 @@ class listaProd(QMainWindow):
                 i += 1
         self.ventana.labelUsuarioEnLogin.setText(self.usuario[1])
         
+    def filaSel(self):
+        self.fila = self.lista.selectedIndexes()
+        if self.fila:
+            self.ventana.btnRevisarProd.setEnabled(True)
+            self.seleccionItem = self.lista.currentRow()
+        else:
+            self.ventana.btnRevisarProd.setEnabled(False)
         
     def Ingresar(self):
-        self.VerificarStock = VerificarStock.Verificar(cont=self.cont)
+        self.VerificarStock = VerificarStock.Verificar(self.seleccionItem, self.cont)
         self.VerificarStock.actualizar()
         self.VerificarStock.show()
-        self.hide()    
+        self.hide()
+    
+    def cerrar(self):
+        self.ventana1.show()
+        self.hide()
       
       
   
